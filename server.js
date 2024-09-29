c08cce5221599b6c819c1fb45c5aa1b95d5b178d
mayanksrivastava1 <100144534+mayanksrivastava1@users.noreply.github.com>
2024-09-29 23:51:26 +0530

    Mongoose changes

--- server.js
+++ server.js
@@ -10,7 +10,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 60000,  // Time in ms before Mongoose gives up trying to connect
+  socketTimeoutMS: 45000,           // Close sockets after 45 seconds of inactivity
+  connectTimeoutMS: 60000,          // Time in ms to allow Mongoose to establish a connection
+  maxPoolSize: 10,                  // Limit the connection pool to 10 connections
+  family: 4,                        // Use IPv4, skip trying to resolve IPv6
+}).then(() => {
+  console.log('MongoDB connected successfully');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
 
 // Import routes
 const userRoutes = require('./Routes/authentication');
